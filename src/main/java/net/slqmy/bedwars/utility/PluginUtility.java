e6071db5ab86c1fa417ca0e7e0003cfd03d477dd
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-07-29 19:29:52 +0100

    📄 Fixed logger and added useful debug utility.

--- src/main/java/net/slqmy/bedwars/utility/PluginUtility.java
+++ src/main/java/net/slqmy/bedwars/utility/PluginUtility.java
@@ -1,5 +1,6 @@
 package net.slqmy.bedwars.utility;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -9,9 +10,13 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public final class PluginUtility {
-	private static final Logger LOGGER = Logger.getLogger("Minecraft");
+	private static final Logger LOGGER = Bukkit.getLogger();
 	private static final String LOG_PREFIX = "[Bedwars]";
 	private static final Pattern FORMAT_PATTERN = Pattern.compile(ChatColor.COLOR_CHAR + "[0-9a-fk-or]");
+
+	public static String getLogPrefix() {
+		return LOG_PREFIX;
+	}
 
 	public static void log(@Nullable final Object message) {
 		final String messageString = clearFormatting(LOG_PREFIX + " " + (message == null ? "NULL" : message.toString()));
