0fa40a2882cbe65104aa7ca3969385f46a050c8b
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-07-08 01:20:11 +0100

    Update to 1.20.6

--- src/main/java/net/slqmy/bedwars/utility/PacketUtility.java
+++ src/main/java/net/slqmy/bedwars/utility/PacketUtility.java
@@ -2,7 +2,7 @@ package net.slqmy.bedwars.utility;
 
 import net.minecraft.network.protocol.game.ClientboundGameEventPacket;
 import net.minecraft.server.level.ServerPlayer;
-import org.bukkit.craftbukkit.v1_20_R1.entity.CraftPlayer;
+import org.bukkit.craftbukkit.v1_20_R4.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
